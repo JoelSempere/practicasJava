6c78f78ae354596828c1ff27070cf71aca102988
Joel Sempere <joelsempere96@gmail.com>
2021-06-16 09:45:58 +0200

    Tercera practica de codewars 7kyu - Organiza en orden Ascendente los números impares y manten los números pares en su misma posición de array

---
+++ practicas/src/CodeWars/sortArray.java
@@ -0,0 +1,52 @@
+package CodeWars;
+
+import java.util.Arrays;
+
+public class sortArray {
+    //Numeros pares en su posicion, impares ordenados orden ASC.
+    public static void main(String[] args) {
+        int[]prueba={71,3,5,1,82,53,12,6,8,15};
+        System.out.println(Arrays.toString(sortArray(prueba)));
+    }
+
+    public static int[] sortArray(int[] array) {
+        int []guardar=new int[array.length];
+
+        int posicion=0;
+        int cont=0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]%2 != 0){
+                guardar[posicion]=array[i];
+                posicion++;
+            }
+        }
+       Arrays.sort(guardar);
+        posicion=0;
+
+        for (int s:guardar
+             ) {
+            if (s !=0){
+                cont++;
+            }
+        }
+        int[]arrayOrdenado=new int[cont];
+
+        for (int s:guardar
+             ) {
+            if (s!=0){
+                arrayOrdenado[posicion]=s;
+                posicion++;
+            }
+        }
+
+        posicion=0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]%2 != 0){
+                array[i]=arrayOrdenado[posicion];
+                posicion++;
+            }
+        }
+        return array;
+    }
+}
